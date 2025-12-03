bc0ccb316e154d0e42bf9ab540383a7dd5c183d5
yrbane <yrbane@nethttp.net>
2025-12-03 11:56:44 +0100

    refactor: add PHPStan level 7 compliance
    - Add PHPDoc type annotations for RouterInterface
    - Apply code style fixes from php-cs-fixer
    
    Closes #2

--- src/Macro/RouterInterface.php
+++ src/Macro/RouterInterface.php
@@ -1,8 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Lunar\Template\Macro;
 
 interface RouterInterface
 {
+    /**
+     * Get route configuration by name.
+     *
+     * @param string $name Route name
+     *
+     * @return array{path: string, params?: array<string, string>}|null Route configuration or null if not found
+     */
     public function getRouteByName(string $name): ?array;
 }
